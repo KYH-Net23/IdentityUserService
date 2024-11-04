3a759b435c0e8719851c7abfc976ab17b88696f9
kahol <kahola@gmail.com>
2024-11-04 09:16:44 +0100

    Solved #2 Task 6. Implemented a database in mysql and created two simple data models for customer and admin.

---
+++ IdentityService/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using IdentityService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Data;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Customer>()
+            .ToTable("Customers");
+
+        builder.Entity<Admin>()
+            .ToTable("Admins");
+    }
+}
---
+++ IdentityService/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Models;
+
+public class Customer : IdentityUser
+{
+    // Add extra
+
+    [StringLength(200, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 200 characters")]
+    public string Address { get; set; } = null!;
+}
