038cfa981f0e21c21b7cff89b7200c7166da4c97
kahol <kahola@gmail.com>
2024-11-11 16:10:05 +0100

    Fixed a customer extension for mapping

---
+++ IdentityService/Extensions/CustomerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using IdentityService.Models.DataModels;
+using IdentityService.Models.FormModels;
+
+namespace IdentityService.Extensions;
+
+public static class CustomerExtensions
+{
+    public static Customer MapToCustomer(this CreateCustomerModel source)
+    {
+        return new Customer
+        {
+            UserName = source.Username,
+            NormalizedUserName = source.Username.ToUpper(),
+            Email = source.Email,
+            NormalizedEmail = source.Email.ToUpper(),
+            PhoneNumber = source.PhoneNumber,
+            StreetAddress = source.StreetAddress,
+            City = source.City,
+            DateOfBirth = source.DateOfBirth,
+            AccountCreationDate = DateTime.Now,
+            IsDeleted = false
+        };
+    }
+}
