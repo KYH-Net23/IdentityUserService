2e8eba73be306760d173564333e0026bb7ae7761
kahol <kahola@gmail.com>
2024-11-11 16:10:26 +0100

    Removed nullable components in CustomerRequestResponse.cs

--- IdentityService/Models/ResponseModels/CustomerRequestResponse.cs
+++ IdentityService/Models/ResponseModels/CustomerRequestResponse.cs
@@ -5,11 +5,11 @@ namespace IdentityService.Models.ResponseModels;
 public class CustomerRequestResponse
 {
 	public string Id { get; set; } = null!;
-	public string? StreetAddress { get; set; }
-	public string? City { get; set; }
+	public string StreetAddress { get; set; } = null!;
+	public string City { get; set; } = null!;
 	public DateTime DateOfBirth { get; set; }
 	public string Email { get; set; } = null!;
-	public string? PhoneNumber { get; set; }
+	public string PhoneNumber { get; set; } = null!;
 	public string Username { get; set; } = null!;
 	public bool IsDeleted { get; set; }
 	public string UserRole => UserRoles.Customer.ToString();
