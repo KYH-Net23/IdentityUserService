ae3bb1a771bed213e97b456eafa11e0ea915a8f5
kahol <kahola@gmail.com>
2024-11-11 16:09:28 +0100

    namespace change

--- IdentityService/Models/FormModels/CreateCustomerModel.cs
+++ IdentityService/Models/FormModels/CreateCustomerModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using IdentityService.Infrastructure;
-using IdentityService.Models;
 
 namespace IdentityService.Models.FormModels;
 
