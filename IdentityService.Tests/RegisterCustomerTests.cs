0aa6a643d566c6ad13cd39cd91484add37310c65
kahol <kahola@gmail.com>
2024-11-12 14:46:01 +0100

    Added tests. No idea what or how or why.

---
+++ IdentityService.Tests/RegisterCustomerTests.cs
@@ -0,0 +1,64 @@
+﻿using IdentityService.Models.FormModels;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace IdentityService.Tests;
+
+public class RegisterCustomerTests : IClassFixture<UserManagerFixture>
+{
+
+    private readonly UserManagerFixture _fixture;
+
+    public RegisterCustomerTests(UserManagerFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task RegisterCustomer_Success()
+    {
+        var userModel = new CreateCustomerModel
+        {
+            Username = "TestUser",
+            Email = "TestUser@gmail.com",
+            Password = "Test123#",
+            PhoneNumber = "123123123",
+            StreetAddress = "Some street address",
+            City = "Some city",
+        };
+
+        _fixture.UserManagerMock
+            .Setup(userManager => userManager.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        var result = await _fixture.CustomerService.RegisterCustomer(userModel);
+
+        Assert.True(result.Succeeded);
+        Assert.Equal("User created", result.Message);
+        Assert.Null(result.Content);
+    }
+
+    [Fact]
+    public async Task RegisterCustomer_Failure()
+    {
+        var userModel = new CreateCustomerModel
+        {
+            Username = "TestUser",
+            Email = "TestUser@gmail.com",
+            Password = "Test123#",
+            PhoneNumber = "123123123",
+            StreetAddress = "Some street address",
+            City = "Some city",
+        };
+
+        _fixture.UserManagerMock
+            .Setup(userManager => userManager.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed());
+
+        var result = await _fixture.CustomerService.RegisterCustomer(userModel);
+
+        Assert.False(result.Succeeded);
+        Assert.Equal("User creation failed", result.Message);
+        Assert.NotNull(result.Content);
+    }
+}
---
+++ IdentityService.Tests/UserManagerFixture.cs
@@ -0,0 +1,35 @@
+﻿using IdentityService.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace IdentityService.Tests;
+
+public class UserManagerFixture : IDisposable
+{
+    public Mock<UserManager<IdentityUser>> UserManagerMock { get; set; }
+    public CustomerService CustomerService { get; set; }
+
+
+    public UserManagerFixture()
+    {
+        UserManagerMock = new Mock<UserManager<IdentityUser>>(
+            new Mock<IUserStore<IdentityUser>>().Object,
+            new Mock<IOptions<IdentityOptions>>().Object,
+            new Mock<IPasswordHasher<IdentityUser>>().Object,
+            Array.Empty<IUserValidator<IdentityUser>>(),
+            Array.Empty<IPasswordValidator<IdentityUser>>(),
+            new Mock<ILookupNormalizer>().Object,
+            new Mock<IdentityErrorDescriber>().Object,
+            new Mock<IServiceProvider>().Object,
+            new Mock<ILogger<UserManager<IdentityUser>>>().Object);
+
+        CustomerService = new CustomerService(UserManagerMock.Object);
+    }
+
+    public void Dispose()
+    {
+        UserManagerMock.Object.Dispose();
+    }
+}
