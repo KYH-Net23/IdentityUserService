17b90afe4faedcbe9f1d5b71639e7b15e00e356e
kahol <kahola@gmail.com>
2024-11-14 16:59:58 +0100

    implemented an Azure Email Sender skeleton. This can be used to send confirmation emails

---
+++ IdentityService/Services/AzureEmailSender.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using Azure.Communication.Email;
+using IdentityService.Factory;
+using IdentityService.Models.ResponseModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json;
+
+namespace IdentityService.Services;
+
+public class AzureEmailSender(
+    HttpClient httpClient,
+    IConfiguration config,
+    UserManager<IdentityUser> userManager
+) : IEmailSender
+{
+    private readonly string _secret = config["EmailProviderSecretKey"]!;
+
+    public async Task Execute(ResponseResult response)
+    {
+        var user = response.Content as IdentityUser;
+        var subject = "";
+        var body = "";
+        await SendEmailAsync(user.Email, subject, body);
+    }
+
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var user = userManager.FindByEmailAsync(email).Result;
+        var confirmationLink = await userManager.GenerateEmailConfirmationTokenAsync(user);
+        var emailMessage = new EmailMessage(
+            "",
+            new EmailRecipients(),
+            new EmailContent(confirmationLink)
+        );
+
+        var accessToken = TokenGeneratorFactory.GenerateAccessToken(_secret);
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Bearer",
+            accessToken
+        );
+
+        var emailContent = new StringContent(
+            JsonConvert.SerializeObject(emailMessage),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        var response = await httpClient.PostAsync("api/Email/SendConfirmationEmail", emailContent);
+
+        response.EnsureSuccessStatusCode();
+    }
+}
