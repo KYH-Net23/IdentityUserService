60c5473dc367a2a3411bf1d755675889a82e0b6b
kahol <kahola@gmail.com>
2024-11-14 16:59:29 +0100

    removed api versions for now

--- IdentityService/Controllers/AdminController.cs
+++ IdentityService/Controllers/AdminController.cs
@@ -1,43 +1,46 @@
-﻿using IdentityService.Services;
+﻿using Asp.Versioning;
+using IdentityService.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityService.Controllers;
 
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
 [ApiController]
 [Route("[controller]")]
-public class AdminController(UserManager<IdentityUser> userManager, AdminService adminService) : ControllerBase
+public class AdminController(UserManager<IdentityUser> userManager, AdminService adminService)
+    : ControllerBase
 {
-	[HttpGet("{email}")]
-	public async Task<IActionResult> GetUser(string email)
-	{
-		var user = await userManager.FindByEmailAsync(email);
-		if (user != null)
-		{
-			return Ok(user.Email + user.UserName);
-		}
+    [HttpGet("{email}")]
+    public async Task<IActionResult> GetUser(string email)
+    {
+        var user = await userManager.FindByEmailAsync(email);
+        if (user != null)
+        {
+            return Ok(user.Email + user.UserName);
+        }
 
-		return BadRequest();
-	}
+        return BadRequest();
+    }
 
-	[HttpGet("GetAdmins")]
-	public async Task<IActionResult> GetAdmins()
-	{
-		var listOfAdmins = await adminService.GetAdmins();
-		return Ok(listOfAdmins);
-	}
+    [HttpGet("GetAdmins")]
+    public async Task<IActionResult> GetAdmins()
+    {
+        var listOfAdmins = await adminService.GetAdmins();
+        return Ok(listOfAdmins);
+    }
 
-	[HttpDelete("delete")]
-	public async Task<IActionResult> DeleteUser()
-	{
-		var userRole = HttpContext.Items["UserRole"] as string;
-		var isAuthenticated = HttpContext.Items["IsAuthenticated"] as bool? ?? false;
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteUser()
+    {
+        var userRole = HttpContext.Items["UserRole"] as string;
+        var isAuthenticated = HttpContext.Items["IsAuthenticated"] as bool? ?? false;
 
-		if (userRole != "Admin" || !isAuthenticated)
-		{
-			return Forbid();
-		}
-		return Ok("Ok. Not implemented yet.");
-	}
+        if (userRole != "Admin" || !isAuthenticated)
+        {
+            return Forbid();
+        }
+        return Ok("Ok. Not implemented yet.");
+    }
 }
-
--- IdentityService/Controllers/CustomerController.cs
+++ IdentityService/Controllers/CustomerController.cs
@@ -1,42 +1,46 @@
-﻿using IdentityService.Models.FormModels;
+﻿using Asp.Versioning;
+using IdentityService.Models.FormModels;
 using IdentityService.Models.ResponseModels;
 using IdentityService.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityService.Controllers;
 
-[Route("[controller]")]
+[ApiVersion("1.0")]
+[ApiVersion("2.0")]
+[Route("/v{version:apiVersion}/[controller]")]
 [ApiController]
-public class CustomerController(CustomerService customerService) : ControllerBase
+public class CustomerController(CustomerService customerService, AzureEmailSender azureEmailSender)
+    : ControllerBase
 {
-	[HttpGet("[action]")]
-	public async Task<IEnumerable<CustomerRequestResponse>> GetCustomers()
-	{
-		return await customerService.GetDemoCustomers();
-	}
+    [HttpGet("[action]")]
+    public async Task<IEnumerable<CustomerRequestResponse>> GetCustomers()
+    {
+        return await customerService.GetDemoCustomers();
+    }
 
-	[HttpPost("[action]")]
-	public async Task<IActionResult> Register([FromBody] CreateCustomerModel registerModel)
-	{
-		if (!ModelState.IsValid)
-		{
-			var errors = ModelState.Values.SelectMany(v => v.Errors);
-			return BadRequest(new {errors});
-		}
-		try
-		{
-			var result = await customerService.RegisterCustomer(registerModel);
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Register([FromBody] CreateCustomerModel registerModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+            return BadRequest(new { errors });
+        }
+        try
+        {
+            var result = await customerService.RegisterCustomer(registerModel);
 
-			if (result.Succeeded)
-			{
-				return Ok(new {result.Message});
-			}
+            if (!result.Succeeded)
+                return BadRequest(new { result.Message, PasswordErrors = result.Content });
 
-			return BadRequest(new {PasswordErrors = result.Content});
-		}
-		catch (Exception e)
-		{
-			return BadRequest(new{e.Message});
-		}
-	}
+            await azureEmailSender.Execute(result);
+            return Ok(new { result.Message });
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { e.Message });
+        }
+    }
 }
