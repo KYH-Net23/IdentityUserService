175a6d457e53de29bb1089b5fe2a945ae362d240
kahol <kahola@gmail.com>
2024-11-15 15:01:49 +0100

    added some demo methods for sending emails

--- IdentityService/Services/AzureEmailSender.cs
+++ IdentityService/Services/AzureEmailSender.cs
@@ -1,10 +1,8 @@
 ﻿using System.Net.Http.Headers;
 using System.Text;
-using Azure.Communication.Email;
-using IdentityService.Factory;
-using IdentityService.Models.ResponseModels;
+using IdentityService.Factories;
+using IdentityService.Models.FormModels;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Newtonsoft.Json;
 
 namespace IdentityService.Services;
@@ -13,42 +11,70 @@ public class AzureEmailSender(
     HttpClient httpClient,
     IConfiguration config,
     UserManager<IdentityUser> userManager
-) : IEmailSender
+)
 {
     private readonly string _secret = config["EmailProviderSecretKey"]!;
 
-    public async Task Execute(ResponseResult response)
+    public async Task SendConfirmationLinkAsync(IdentityUser user)
     {
-        var user = response.Content as IdentityUser;
-        var subject = "";
-        var body = "";
-        await SendEmailAsync(user.Email, subject, body);
-    }
-
-    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
-    {
-        var user = userManager.FindByEmailAsync(email).Result;
         var confirmationLink = await userManager.GenerateEmailConfirmationTokenAsync(user);
-        var emailMessage = new EmailMessage(
-            "",
-            new EmailRecipients(),
-            new EmailContent(confirmationLink)
-        );
-
         var accessToken = TokenGeneratorFactory.GenerateAccessToken(_secret);
+
         httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
             "Bearer",
             accessToken
         );
 
         var emailContent = new StringContent(
-            JsonConvert.SerializeObject(emailMessage),
+            JsonConvert.SerializeObject(new { email = user.Email, confirmationLink }),
             Encoding.UTF8,
             "application/json"
         );
 
-        var response = await httpClient.PostAsync("api/Email/SendConfirmationEmail", emailContent);
+        var response = await httpClient.PostAsync("/IdentityEmail", emailContent);
 
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task TestMethod(EmailRequestModel email)
+    {
+        var accessToken = TokenGeneratorFactory.GenerateAccessToken(_secret);
+
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Bearer",
+            accessToken
+        );
+
+        var emailContent = new EmailRequestModel { Receiver = email.Receiver, Uri = email.Uri, };
+
+        var content = new StringContent(
+            JsonConvert.SerializeObject(emailContent),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        using var response = await httpClient.PostAsync("/IdentityEmail", content);
+        response.EnsureSuccessStatusCode();
+    }
+
+    public async Task ResetEmail(EmailRequestModel email)
+    {
+        var accessToken = TokenGeneratorFactory.GenerateAccessToken(_secret);
+
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
+            "Bearer",
+            accessToken
+        );
+
+        var emailContent = new EmailRequestModel { Receiver = email.Receiver, Uri = email.Uri, };
+
+        var content = new StringContent(
+            JsonConvert.SerializeObject(emailContent),
+            Encoding.UTF8,
+            "application/json"
+        );
+
+        using var response = await httpClient.PostAsync("/IdentityEmail", content);
         response.EnsureSuccessStatusCode();
     }
 }
