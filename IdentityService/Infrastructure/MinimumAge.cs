1e290e457573f3fc3f70e00c863131a2ad9b9593
kahol <kahola@gmail.com>
2024-11-29 08:56:28 +0100

    fix

--- IdentityService/Infrastructure/MinimumAge.cs
+++ IdentityService/Infrastructure/MinimumAge.cs
@@ -4,18 +4,16 @@ namespace IdentityService.Infrastructure;
 
 public class MinimumAge : ValidationAttribute
 {
-	private const int MinAge = 18;
+    private const int MinAge = 18;
 
-	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-	{
-		if (value is not DateTime birthDay)
-			return new ValidationResult("Please enter a valid date format.");
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not DateTime birthDay)
+            return new ValidationResult("Please enter a valid date format.");
 
-		if (birthDay.AddYears(MinAge) <= DateTime.Now)
-		{
-			return ValidationResult.Success;
-		}
+        if (birthDay.AddYears(MinAge) <= DateTime.Now)
+            return ValidationResult.Success;
 
-		return new ValidationResult("You have to be at least 18 years old to register.");
-	}
+        return new ValidationResult("You have to be at least 18 years old to register.");
+    }
 }
